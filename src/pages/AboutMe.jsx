0fc0944f1a560b82d63b225f6df546000e48555b
zeeyakanjiyani <106193843+zeeyakanjiyani@users.noreply.github.com>
2023-07-07 14:07:46 -0500

    added brief intro to AboutMe page

--- src/pages/AboutMe.jsx
+++ src/pages/AboutMe.jsx
@@ -1,7 +1,24 @@
+import selfImage from "../assets/self-image.jpg";
+import {Button, Container, Row, Col } from "react-bootstrap";
 function AboutMe(){
     return(
         <div>
-            Hello AboutMe Page!
+
+            <br></br>
+            {/* add a picture of yourself */}
+            <img src={selfImage} alt="myself" style={{alignItems:"center", width:"20%"}}></img>
+            <br></br>
+            <br></br>
+            <body style={{color:"white"}}>
+                Hello! My name is Zeeya Kanjiyani and I'm a student at the University of Texas at Austin.
+                <br></br>
+                I am currently in my 3rd year pursuing a B.S. in Chemical Engineering and a minor in Computer Science.
+                {/* insert resume button */}
+                {/* download button and open in a new tab button */}
+            </body>
+            <br></br>
+            <header style={{color:"white"}}>Academic & Professional</header>
+            
         </div>
     );
 }
