b43123cccdd92a5e1c5b09d45525def34f1ecad7
zeeyakanjiyani <106193843+zeeyakanjiyani@users.noreply.github.com>
2023-07-22 10:16:50 -0500

    redid NavBar using styled components instead of CSS

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,16 +1,36 @@
+
+import React from "react";
+import {Nav, Container, NavDropdown, Navbar} from 'react-bootstrap';
 import "../components/NavBar.css";
-// import { Nav } from "react-bootstrap";
 
-function NavBar(){
+function GlobalNavBar(){
     return(
-
-        <ul>
-            <li><a href="/">Home</a></li>
-            <li><a href="./AboutMe">About Me</a></li>
-            <li><a href="/Projects">Projects</a></li>
-            <li><a href="/Contact">Contact</a></li>
-        </ul>
+        <Navbar sticky="top" >
+            <Container>
+                <Nav>
+                    <Nav.Link style={{color:"white"}} href="/">Home</Nav.Link>
+                    <NavDropdown title={<span className="text-title-color align-content-center">About</span>} id="basic-nav-dropdown">
+                        <NavDropdown.Item href="/Personal">Personal</NavDropdown.Item>
+                        <NavDropdown.Item href="/Professional">Professional</NavDropdown.Item>
+                        <NavDropdown.Divider/>
+                        <NavDropdown.Item href="/Contact">Contact</NavDropdown.Item>
+                    </NavDropdown>
+                    <Nav.Link style={{color:"white"}} href="/Projects">Projects</Nav.Link>
+                </Nav>
+            </Container>
+        </Navbar>
+        
+        // <ul>
+        //      <li><a href="/">Home</a></li>
+        //      <li><NavDropdown title="About" id="basic-nav-dropdown">
+        //         <NavDropdown.Item href="/Personal">Personal</NavDropdown.Item>
+        //         <NavDropdown.Item href="/Professional">Professional</NavDropdown.Item>
+        //      </NavDropdown></li>
+        //      <li><a href="/Projects">Projects</a></li>
+        //      <li><a href="/Contact">Contact</a></li>
+        //  </ul>
+           
     )
 }
 
-export default NavBar;
+export default GlobalNavBar;
