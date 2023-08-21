fbe5d617e3a3a92407bbb2b348cc66de87dcfc6a
zeeyakanjiyani <106193843+zeeyakanjiyani@users.noreply.github.com>
2023-08-21 14:53:52 -0500

    added tooltip and copy to clipboard function for Contact button

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,6 +1,6 @@
 
 import React from "react";
-import {Nav, Container, NavDropdown, Navbar, Dropdown} from 'react-bootstrap';
+import {Nav, Container, NavDropdown, Navbar, Dropdown, OverlayTrigger, Tooltip} from 'react-bootstrap';
 import "../components/custom.css";
 
 function GlobalNavBar(){
@@ -14,26 +14,16 @@ function GlobalNavBar(){
                         <NavDropdown.Item href="/Professional">Professional</NavDropdown.Item>
                         <NavDropdown.Divider style={{backgroundColor:"white"}}/>
                         <NavDropdown key="end" drop="end" title={<span className="text-title-color align-content-center">Contact</span>}>
-                            <Dropdown.Item href="https://www.linkedin.com/in/zeeyakanjiyani/" target="_blank">LinkedIn</Dropdown.Item>
-                            <Dropdown.Item href="https://github.com/zeeyakanjiyani" target="_blank">GitHub</Dropdown.Item>
-                            <Dropdown.Item >E-mail</Dropdown.Item>
+                            <Dropdown.Item href="https://www.linkedin.com/in/zeeyakanjiyani/" target="_blank" rel="noreferrer">LinkedIn</Dropdown.Item>
+                            <Dropdown.Item href="https://github.com/zeeyakanjiyani" target="_blank" rel="noreferrer">GitHub</Dropdown.Item>
+                            <OverlayTrigger key="bottom" placement="bottom" overlay={<Tooltip id="tooltip-bottom">Click to copy</Tooltip>}>
+                                <Dropdown.Item style={{onClick:navigator.clipboard.writeText("zeeyakanjiyani@gmail.com")}}>zeeyakanjiyani@gmail.com</Dropdown.Item>
+                            </OverlayTrigger>
                         </NavDropdown>
                     </NavDropdown>
-                    <Nav.Link style={{color:"white"}} href="/Projects">Projects</Nav.Link>
                 </Nav>
             </Container>
-        </Navbar>
-        
-        // <ul>
-        //      <li><a href="/">Home</a></li>
-        //      <li><NavDropdown title="About" id="basic-nav-dropdown">
-        //         <NavDropdown.Item href="/Personal">Personal</NavDropdown.Item>
-        //         <NavDropdown.Item href="/Professional">Professional</NavDropdown.Item>
-        //      </NavDropdown></li>
-        //      <li><a href="/Projects">Projects</a></li>
-        //      <li><a href="/Contact">Contact</a></li>
-        //  </ul>
-           
+        </Navbar>    
     )
 }
 
